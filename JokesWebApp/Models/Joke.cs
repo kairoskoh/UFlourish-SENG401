d483da8a26b7fcb5a7bc01c1a6ab062b31e58e01
LAPTOP-H8FATR55\kairo <kairoskoh.1212@gmail.com>
2023-02-16 07:30:20 +0800

    Add project files.

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        // PROP TAB TAB - PROPERTY
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        // CTOR TAB TAB - CONSTRUCTOR
+        public Joke()
+        {
+
+        }
+    }
+}
