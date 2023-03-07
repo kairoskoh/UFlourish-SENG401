a68bead21cfc4a938e47880ca80067e6c7d093a6
LAPTOP-H8FATR55\kairo <kairoskoh.1212@gmail.com>
2023-03-08 07:49:25 +0800

    Add UserFinancialActivity Model, View and Controller

---
+++ JokesWebApp/Controllers/UserFinancialActivitiesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using JokesWebApp.Data;
+using JokesWebApp.Models;
+
+namespace JokesWebApp.Controllers
+{
+    public class UserFinancialActivitiesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserFinancialActivitiesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UserFinancialActivities
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.UserFinancialActivity.ToListAsync());
+        }
+
+        // GET: UserFinancialActivities/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userFinancialActivity = await _context.UserFinancialActivity
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userFinancialActivity == null)
+            {
+                return NotFound();
+            }
+
+            return View(userFinancialActivity);
+        }
+
+        // GET: UserFinancialActivities/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserFinancialActivities/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserId,TransactionName,TransactionType,PostedDate,Term,Charge,Payment,Refund")] UserFinancialActivity userFinancialActivity)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userFinancialActivity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userFinancialActivity);
+        }
+
+        // GET: UserFinancialActivities/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userFinancialActivity = await _context.UserFinancialActivity.FindAsync(id);
+            if (userFinancialActivity == null)
+            {
+                return NotFound();
+            }
+            return View(userFinancialActivity);
+        }
+
+        // POST: UserFinancialActivities/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,TransactionName,TransactionType,PostedDate,Term,Charge,Payment,Refund")] UserFinancialActivity userFinancialActivity)
+        {
+            if (id != userFinancialActivity.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userFinancialActivity);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserFinancialActivityExists(userFinancialActivity.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userFinancialActivity);
+        }
+
+        // GET: UserFinancialActivities/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userFinancialActivity = await _context.UserFinancialActivity
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userFinancialActivity == null)
+            {
+                return NotFound();
+            }
+
+            return View(userFinancialActivity);
+        }
+
+        // POST: UserFinancialActivities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var userFinancialActivity = await _context.UserFinancialActivity.FindAsync(id);
+            _context.UserFinancialActivity.Remove(userFinancialActivity);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserFinancialActivityExists(int id)
+        {
+            return _context.UserFinancialActivity.Any(e => e.Id == id);
+        }
+    }
+}
