1754916888606ca3d1746819e7a279eb725fe7d9
LAPTOP-H8FATR55\kairo <kairoskoh.1212@gmail.com>
2023-03-23 06:38:28 +0800

    Add fix to missing database table

--- JokesWebApp/Data/Migrations/20230307232706_setup user-financial-activity.cs
+++ JokesWebApp/Data/Migrations/20230307232706_setup user-financial-activity.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace JokesWebApp.Data.Migrations
 {
@@ -25,12 +26,27 @@ namespace JokesWebApp.Data.Migrations
                 {
                     table.PrimaryKey("PK_UserFinancialActivity", x => x.Id);
                 });
+            migrationBuilder.CreateTable(
+                name: "Request_Mental_Health",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Request_Mental_Health", x => x.Id);
+                });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
                 name: "UserFinancialActivity");
+            migrationBuilder.DropTable(
+                name: "Request_Mental_Health");
         }
     }
 }
