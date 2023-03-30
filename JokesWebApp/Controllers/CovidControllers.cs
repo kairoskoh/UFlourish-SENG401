115039ea4ee3001f010cc0808145cecf547f90c1
simratbenipal <117254852+simratbenipal@users.noreply.github.com>
2023-03-29 19:41:06 -0600

    covid screening working from home
    everything for covid is in home controller, working as the alerts
    minimial design so far, can add more symptoms.

---
+++ JokesWebApp/Controllers/CovidControllers.cs
@@ -0,0 +1,42 @@
+﻿using JokesWebApp.Data;
+using JokesWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+namespace JokesWebApp.Controllers
+{
+    public class CovidController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public IActionResult ScreeningForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ScreeningForm(ScreeningFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["SuccessMessage"] = "Please screenshot and show this at the front-desk";
+                return RedirectToAction("ScreeningFormSuccess");
+            }
+
+            return View(model);
+        }
+
+        public IActionResult ScreeningFormSuccess()
+        {
+            var successMessage = TempData["SuccessMessage"] as string;
+            ViewBag.SuccessMessage = successMessage;
+            return View();
+        }
+    }
+
+
+}
